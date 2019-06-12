6ad5ee6642d3a9ae8d1bb9327911d476e350d262
SeJ19 <42857617+SeJ19@users.noreply.github.com>
2019-06-12 13:11:13 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,25 +2,26 @@
 #include<conio.h>
 void main()
 {  clrscr();
-   double a,b,result=0;
-   int choice;
+   int a,b;
+   double result=0;
+   char choice;
    printf("Enter two numbers: ");
-   scanf("%lf%lf", &a,&b);
-   printf("Enter your choice(1-4): ");
-   printf("\n1 for Addition.\n2 for Subtraction.\n3 for Multiplication.");
-   printf("\n4 for Division.\n");
-   scanf("%d",&choice);
+   scanf("%d%d", &a,&b);
+   printf("Enter your choice: ");
+   printf("\n + for Addition.\n - for Subtraction.\n * for Multiplication.");
+   printf("\ / for Division.\n");
+   scanf("%c",&choice);
    switch(choice){
-case 1: result = a+b;
+case +: result = a+b;
 	printf("\nThe sum of %lf and %lf is: %lf",a,b,result);
 	break;
-case 2: result = a-b;
+case -: result = a-b;
 	printf("\nThe difference of %lf and %lf is: %lf",a,b,result);
 	break;
-case 3: result = a*b;
+case *: result = a*b;
 	printf("\nThe product of %lf and %lf is: %lf",a,b,result);
 	break;
-case 4: result = a/b;
+case /: result = a/b;
 	printf("\nThe quotient of %lf and %lf is: %lf",a,b,result);
 	break;
 default:"invalid choice";
